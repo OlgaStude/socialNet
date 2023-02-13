3e3a2880087754c0ecb9f84e02baf42606e484a7
OlgaStude <indimindy@yandex.ru>
2023-02-13 19:32:53 +0400

    lesson 3

---
+++ app/Http/Controllers/Api/authController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class authController extends Controller
+{
+    public function login(){
+
+    }
+
+    public function register(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string | max:255',
+            'email' => 'required|string| email | max:255 | unique:users',
+            'password' => 'required| between:8, 255 | confirmed'
+        ]);
+        if($validator->fails()){
+            return response(['errors' => $validator->errors()->all()], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if (!$user){
+            return response()->json(['succsess' => false, 'message' => 'reg failed'], 500);
+        }
+
+        return response()->json(['succsess' => true, 'message' => 'reg succeded'], 200);
+    }
+}
