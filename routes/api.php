877d176fa12e3dde7e46e57360a1d6f28b8241b9
OlgaStude <indimindy@yandex.ru>
2023-02-13 21:20:10 +0400

    lesson 4

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register', [authController::class, 'register']);
+Route::post('login', [authController::class, 'login']);
